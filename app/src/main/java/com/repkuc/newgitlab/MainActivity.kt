5a743d0aaf219b3a800a91cfc6e2f3f77b8aa724
repkuc <arturs.gerkis@inbox.lv>
2021-12-28 19:31:15 +0200

    textview deleted textview lateinit var add

--- app/src/main/java/com/repkuc/newgitlab/MainActivity.kt
+++ app/src/main/java/com/repkuc/newgitlab/MainActivity.kt
@@ -2,8 +2,13 @@ package com.repkuc.newgitlab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var arr: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
